f74d9874fb5a0343a6b9c0a8c7a6a80ca45365b9
chlee7139 <chlee7139@gmail.com>
2022-03-09 00:00:03 +0900

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+import org.bukkit.ChatColor
+import org.bukkit.plugin.java.JavaPlugin
+import task.runTaskTimer
+
+class Main : JavaPlugin() {
+
+    override fun onEnable() {
+        server.consoleSender.sendMessage("${ChatColor.GREEN}플러그인이 활성화되었습니다.")
+    }
+
+    override fun onDisable() {
+        server.consoleSender.sendMessage("${ChatColor.RED}플러그인이 비활성화되었습니다.")
+    }
+}
